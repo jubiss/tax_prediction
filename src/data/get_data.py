a659ffaccdbb79dba1668de9dc8abbccb0b5a8c8
jose.ferreira <jose.edivaldo.fisica@gmail.com>
2023-07-25 10:00:57 -0300

    Adjusting get_data

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -4,75 +4,117 @@ class get_data():
     """"
     Class constructor to retrieve data from SQL Databases and saves in the raw data folders.
     """
-    def __init__(self, database):
+    def __init__(self, credentials):
         """
-        Inform the type of database, allowed databases 'mysql'
+        Inform the credentails and the database that will be used, allowed databases 'mysql'
 
         parameters:
-        database: str
+        credentials: dict
         """
-        self.database = database
-        return self
+        self.credentials = credentials
     
-    def read_data(self, credentials, query, get_column_names=False, table_name=None):
-        if self.database == 'mysql':
-            import mysql.connector
-            # Create a connection to the database
-            #example of parameters
-            """cnx = mysql.connector.connect(
-                host='interview-2.ck1h5ksgzpiq.us-east-1.rds.amazonaws.com',
-                port=3306,
-                user='hotinterview',
-                password='6cT4jk9QWPhQC9KXWKDd',
-                database='innodb'
-            )"""
-            cnx = mysql.connector.connect(
-                host=credentials['host'],
-                port=credentials['port'],
-                user=credentials['user'],
-                password=credentials['password'],
-                database=credentials['database']
-            )
+    def save_data(self, query, get_column_names=False, table_name=None):
+            cnx = self.get_connection()
 
             # Create a cursor object to interact with the database
             cursor = cnx.cursor()
 
             if get_column_names:
-                # Execute a sample query to get the column names
-                query = f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}'"
-                cursor.execute(query)
-
-                # Fetch all column names from the result
-                columns = [column[0] for column in cursor.fetchall()]
-
+                columns = self.column_names(cursor=cursor, table_name=table_name)
                 # Execute a query to fetch the table data
                 query = f"SELECT * FROM {table_name}"
                 cursor.execute(query)
 
+            else:
+                cursor.execute(query)
             # Fetch all rows from the result
             rows = cursor.fetchall()
 
-            # Transform in Dataframe
-            df = pd.DataFrame(rows, columns=columns)
-            # Save table data to a CSV file
-
-            if table_name:
-                df.to_csv(r'\data\raw\{table_name}.csv')
+            if credentials['charset'] != None:
+                # Decode the column values
+                decoded_rows = self.decode(rows)
+                # Transform in Dataframe
+                df = pd.DataFrame(decoded_rows, columns=columns)
             else:
-                df.to_csv(r'\data\raw\table.csv')
+                # Transform in Dataframe
+                df = pd.DataFrame(rows, columns=columns)
+  
+            # Save table data to a CSV file
+            if table_name:
+                df.to_csv(fr'data\raw\{table_name}.csv')
+            else:
+                df.to_csv(r'data\raw\table.csv')
             # Close the cursor and connection
             cursor.close()
             cnx.close()
 
+    def get_connection(self):
+        if self.credentials['database'] == 'mysql':
+            import mysql.connector
+            cnx = mysql.connector.connect(
+                host=self.credentials['host'],
+                port=self.credentials['port'],
+                user=self.credentials['user'],
+                password=self.credentials['password'],
+                database=self.credentials['database_path'],
+            )
+        if self.credentials['database'] == 'fdb':
+            import fdb
+            cnx = fdb.connect(
+                host=self.credentials['host'],
+                port=self.credentials['port'],
+                user=self.credentials['user'],
+                password=self.credentials['password'],
+                database=self.credentials['database_path'],
+                charset=self.credentials['charset']
+            )
+        return cnx
+
+    def decode(self, rows):
+        if self.credentials['database'] == 'fdb':
+            decoded_rows = []
+            for row in rows:
+                decoded_row = []
+                for value in row:
+                    if isinstance(value, bytes):
+                        try:
+                            decoded_value = value.decode(self.credentials['charset'])
+                        except UnicodeDecodeError:
+                            decoded_value = value.decode(self.credentials['charset'], errors='replace')
+                        decoded_row.append(decoded_value)
+                    else:
+                        decoded_row.append(value)
+                decoded_rows.append(decoded_row)
+        return decoded_rows
+    
+    def column_names(self, cursor, table_name):
+        # Execute a sample query to get the column names
+        if self.credentials['database'] == 'mysql':
+            query = f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}'"
+        if self.credentials['database'] == 'fdb':
+            query = f"SELECT rdb$field_name FROM rdb$relation_fields WHERE rdb$relation_name = '{table_name}'"
+
+        cursor.execute(query)
+
+        # Fetch all column names from the result
+        if (credentials['charset'] != None) and (credentials['database'] == 'fdb'):
+            columns = [column[0].strip() for column in self.decode(cursor.fetchall())]
+        else:
+            columns = [column[0].strip() for column in cursor.fetchall()]
+        return columns
+    
 if __name__ == "__main__":
-    database = 'mysql'
-    credentials = {'host': None,
+    credentials = {'database': 'fdb',
+                   'host': 'localhost',
                    'port': None,
-                   'user': None,
-                   'password': None,
-                   'database': None}
-    query = " "
-    get_column_names=False
-    table_name = None
-    data = get_data(database=database)
-    data.read_data(credentials=credentials, query=query, get_column_names=get_column_names, table_name=table_name)
+                   'user': 'sysdba',
+                   'password': 'masterkey',
+                   'database_path': r'C:\Users\jubi\Desktop\Projetos\projetos proissionais\juliana_comercial_returns\data\BM.FDB',
+                   'charset': 'ISO8859_1'}
+    query = ""
+    get_column_names=True
+    table_names = ['PRODUTO', 'ENTRADA_PRODUTO', 'ENTRADA', 'PESSOA']
+    data = get_data(credentials=credentials)
+    for table_name in table_names:
+        print(table_name)
+        data.save_data(query=query, get_column_names=get_column_names, table_name=table_name)
