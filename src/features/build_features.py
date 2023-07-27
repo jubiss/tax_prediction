a973f4f42c12a3e7c6db7ec10e0a7ace60e5152b
jose.ferreira <jose.edivaldo.fisica@gmail.com>
2023-07-27 10:11:51 -0300

    Added PivotPipeline

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,4 +1,37 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+import sys,os
+sys.path.append(os.getcwd())
+from src.utils import utils
+
+class PivotPipeline():
+    def __init__(self, index, columns, values, aggfunc, fill_value=None, prefix_text=None, sufix_text=None):
+        self.INDEXES = index
+        self.COLUMNS = columns
+        self.VALUES = values
+        self.AGGFUNC = aggfunc
+        self.FILL_VALUE = fill_value
+        self.PREFIX_TEXT = prefix_text
+        self.SUFIX_TEXT = sufix_text
+        
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X, y):
+        X = X.reset_index()
+        X = X.pivot_table(index=self.INDEXES,
+                        columns=self.COLUMNS,
+                        values=self.VALUES,
+                        aggfunc=self.AGGFUNC,
+                        fill_value=self.FILL_VALUE,
+                        )
+        X = utils.append_column_names(X, prefix_text=self.PREFIX_TEXT, sufix_text=self.SUFIX_TEXT).reset_index()
+        X = X.set_index('index').sort_index()
+        y = y.drop_duplicates().sort_index()        
+        return X, y
+    
+    def fit_transform(self, X, y):
+        return self.fit(X, y).transform(X, y)
 
 class BuildFeatures_CV(BaseEstimator, TransformerMixin):
     """"
@@ -24,6 +57,4 @@ class BuildFeatures(BaseEstimator, TransformerMixin):
         return self
     
     def transform(self, X, y=None):
-        return X
-    
-    
+        return X, y
